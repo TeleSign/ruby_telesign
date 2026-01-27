575fa9d6a6caa0b9cd7def5af154d18c6d6c4aa5
Mousumi Mohanty <mmohanty@telesign.com>
2026-01-27 22:04:04 +0000

    Pull request #8: feature/EOA-5905-SS-Intellegence-Cloud-Api
    Merge in SDK/ruby_telesign from feature/EOA-5905-SS-Intellegence-Cloud-Api to developer
    
    Squashed commit of the following:
    
    commit 0e312253b127aa0405b12fb834883b3f196f1c51
    Merge: 4a6b1f5 3a548ed
    Author: Juan Camilo Martinez <jmartinez@telesign.com>
    Date:   Tue Jan 27 16:59:08 2026 -0500
    
        Fix merge
    
    commit 4a6b1f5bde9013d4a565d3edf0e983fbe54e843b
    Author: MousumiMohanty <@telesign.com>
    Date:   Fri Nov 14 13:16:14 2025 +0530
    
        Added support for Intelligence Cloud to use new endpoint

--- examples/score/1_check_phone_number_risk_level.rb
+++ examples/score/1_check_phone_number_risk_level.rb
@@ -1,17 +1,21 @@
 require 'telesign'
 
 customer_id = 'FFFFFFFF-EEEE-DDDD-1234-AB1234567890'
-api_key = 'EXAMPLE----TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw=='
+api_key = 'ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw=='
 
-phone_number = 'phone_number'
+phone_number = '11234567890'
 account_lifecycle_event = 'create'
 
 score_client = Telesign::ScoreClient.new(customer_id, api_key)
 response = score_client.score(phone_number, account_lifecycle_event)
 
 if response.ok
-    puts "Phone number %s has a '%s' risk level and the recommendation is to '%s' the transaction." %
-        [phone_number,
-        response.json['risk']['level'],
-        response.json['risk']['recommendation']]
+  puts format(
+    "Phone number %s has a '%s' risk level and the recommendation is to '%s' the transaction.",
+    phone_number,
+    response.json['risk']['level'],
+    response.json['risk']['recommendation']
+  )
+else
+  puts "Request failed with status code: #{response.status_code}. Details: #{response.json}"
 end
